e3ff8af7dae190a97e363ccf0af8cf0e7bab321e
henteko <henteko07@gmail.com>
2015-11-03 20:47:56 +0900

    finish

--- lib/face_counter.rb
+++ lib/face_counter.rb
@@ -1,5 +1,22 @@
-require "face_counter/version"
+require "opencv"
 
 module FaceCounter
-  # Your code goes here...
+  class << self
+    def run
+      if ARGV.size < 1
+        STDERR.puts " $ #{$0} input.jpg"
+        exit 1
+      end
+
+      input_filename = ARGV.shift
+
+      image = OpenCV::IplImage::load input_filename
+      haar_xml_file = File.join(File.dirname(__FILE__), 'haarcascade_frontalface_alt.xml')
+      detector = OpenCV::CvHaarClassifierCascade::load haar_xml_file
+
+      p detector.detect_objects(image).count
+    end
+  end
 end
+
+require "face_counter/version"
